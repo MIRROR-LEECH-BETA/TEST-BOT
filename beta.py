8b1207a003156a78dfd9759cdb0c67d6950ff51e
JEOL HERE! <102511853+Jeolpaul@users.noreply.github.com>
2022-08-11 08:17:49 +0530

    Update beta.py

--- beta.py
+++ beta.py
@@ -7,9 +7,6 @@ from pyrogram.types import *
 from database import *
 import re
 import os
-from helper.database import getid
-
-id_pattern = re.compile(r'^.\d+$')
 
 #os.system("pip install ffmpeg -y")
 
@@ -40,28 +37,7 @@ async def hello(client: ZAID, message: Message):
 
 
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
 
-@ZAID.on_message(filters.private & filters.user(ADMIN) & filters.command(["broadcast"]))
-async def broadcast(bot, message):
- if (message.reply_to_message):
-   ms = await message.reply_text("Geting All ids from database ...........")
-   ids = getid()
-   tot = len(ids)
-   await ms.edit(f"Starting Broadcast .... \n Sending Message To {tot} Users")
-   for id in ids:
-     try:
-     	await message.reply_to_message.copy(id)
-     except:
-     	pass
-
-
-@ZAID.on_message(filters.private & filters.user(ADMIN) & filters.command(["users"]))
-async def get_users(client: Client, message: Message):
-    msg = await client.send_message(chat_id=message.chat.id, text="weit....")
-    ids = getid()
-    tot = len(ids)
-    await msg.edit(f"Total uses = {tot}")
 
 
 ##Copy from here 
@@ -77,7 +53,7 @@ async def clone(bot: ZAID, msg: Message):
     try:
         m = await text.edit("Developing Your Bot")
                    # change this Directry according to ur repo
-        client = Client( ":memory:", API_ID, API_HASH, bot_token=phone, in_memory=True, plugins={"root": "plugins"})
+        client = Client( ":memory:", API_ID, API_HASH, bot_token=phone, in_memory=True, plugins={"root": "bot"})
         await client.start()
         idle()
         user = await client.get_me()
@@ -92,7 +68,7 @@ for kk in op:
     nam = [kk['bot_token']]
     for usr in nam:
         print(usr)
-        app = Client("cache",api_id=API_ID, api_hash=API_HASH, bot_token=usr ,in_memory=True, plugins={"root": "plugins"})
+        app = Client("cache",api_id=API_ID, api_hash=API_HASH, bot_token=usr ,in_memory=True, plugins={"root": "bot"})
         app.start()
 
 
